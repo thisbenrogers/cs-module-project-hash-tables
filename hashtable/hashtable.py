2b4fb82e790a937ef10b8c2443e273fb52757690
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-09-04 19:47:27 -0500

    Committing to troubleshoot

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -7,6 +7,68 @@ class HashTableEntry:
         self.value = value
         self.next = None
 
+    def __str__(self):
+        return f"{self.value}"
+
+    def __repr__(self):
+        return  (f"HashTableEntry("
+                f"\n\tkey={self.key}"
+                f"\n\tvalue={self.value}\n)")
+
+    def get_value(self):
+        return self.value
+    def get_next(self):
+        return self.next
+    def set_next(self, new_next):
+        self.next = new_next
+
+# Linked List to manage chaining collisions
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def __str__(self):
+        return f"length: {self.length}"
+
+    def __repr__(self):
+        return  (f"LinkedList("
+                f"\n\thead={self.head}"
+                f"\n\tlength={self.length}\n)")
+
+    def find(self, key):
+        current_entry = self.head
+        while current_entry is not None:
+            if key == current_entry.key:
+                return current_entry.value
+            else: 
+                current_entry == current_entry.next
+        return None
+
+    
+    def add_to_head(self, key, value):
+        new_entry = HashTableEntry(key, value, self.head)
+        self.head = new_entry
+        self.length += 1
+
+    def delete_entry(self, key):
+        temp = self.head
+        if temp is not None and temp.key == key:
+            self.head = temp.next
+            temp = None
+            return
+        while temp is not None:
+            if temp.key == key:
+                break
+            prev = temp
+            temp = temp.next
+        if temp == None:
+            print('Not Found')
+            return
+        prev.next = temp.next
+        temp = None
+        self.length -= 1
+
 
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
@@ -22,9 +84,11 @@ class HashTable:
 
     def __init__(self, capacity):
         if capacity < MIN_CAPACITY:
-            capacity = MIN_CAPACITY
-        self.capacity = capacity
+            self.capacity = MIN_CAPACITY
+        else:
+            self.capacity = capacity
         self.storage = [None] * capacity
+        self.count = 0
 
 
     def get_num_slots(self):
@@ -39,16 +103,13 @@ class HashTable:
         """
         return len(self.storage)
 
-
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
 
         Implement this.
         """
-        # return self.count / self.get_num_slots()
-        pass
-
+        return self.count / self.get_num_slots()
 
     def fnv1(self, key):
         """
@@ -64,8 +125,6 @@ class HashTable:
             FNV_offset_basis = FNV_offset_basis ^ ord(x)
         return FNV_offset_basis
         
-
-
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
@@ -77,7 +136,6 @@ class HashTable:
         for x in key:
             DJB2_hash = ((DJB2_hash << 5) + DJB2_hash) + ord(x)
         return DJB2_hash & 0xFFFFFFFF
-
 
     def hash_index(self, key):
         """
@@ -96,8 +154,20 @@ class HashTable:
         Implement this.
         """
         i = self.hash_index(key)
-        self.storage[i] = HashTableEntry(key, value)
+        if self.storage[i] is None:
+            new_list = LinkedList()
+            new_list.add_to_head(key, value)
+            self.storage.insert(i, new_list)
+        else:
+            old_list = self.storage[i]
+            old_list.add_to_head(key, value)
+            print(f"old_list: {old_list} -- head: {old_list.head} -- next: {old_list.head.next}")
+        self.count += 1
 
+        ### Test for resizing
+        # if (self.get_load_factor() > 0.7):
+            # Double size of Array
+            # Then copy everything over, one at a time, re-hashing again
 
     def delete(self, key):
         """
@@ -108,8 +178,15 @@ class HashTable:
         Implement this.
         """
         i = self.hash_index(key)
-        self.storage[i] = None
+        linked_list = self.storage[i]
+        linked_list.delete_entry(key)
+        self.count -= 1
 
+        ### Test for resizing
+        # if (self.get_load_factor() < 0.2):
+            # THIS IS A STRETCH GOAL
+            # Cut the Array down in half
+            # Then copy everything over, one at a time, re-hashing again
 
     def get(self, key):
         """
@@ -120,12 +197,15 @@ class HashTable:
         Implement this.
         """
         i = self.hash_index(key)
-        text = self.storage[i]
-        if text:
-            return text.value
+        print(f"i {i}")
+        if self.storage[i] is None:
+            return None
+        linked_list = self.storage[i]
+        text = linked_list.find(key)
+        if text is None:
+            return None
+        return text
         
-
-
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
@@ -133,8 +213,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        pass
 
 
 if __name__ == "__main__":
