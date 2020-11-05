980ea30a2b0c6b0bee7bca78267bc7de5cd6f8a9
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-11-05 16:55:05 -0600

    Implements expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,17 @@
 # Your code here
 
+table = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
-
+    if (x, y, z) in table:
+        return table[(x, y, z)]
+    else:
+        if x <= 0: 
+            a = y + z
+        else: 
+            a = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)   
+        table[(x, y, z)] = a
+        return a
 
 
 if __name__ == "__main__":
